7e9840899a1cbf1be02ee6d1033248ebd7a32f5b
William Nogueira <will.ribeiro@live.com>
2022-04-20 17:16:03 -0300

    Utilizando o Principio de substituição de Liskov (LSP)

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,12 +1,10 @@
 import { Category } from '../model/Category';
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from './ICategoriesService';
 
-// DTO => data transfer object
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
-
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
   constructor() {
