91633e32ba7c381bbb0948e9fe86942eb7932eef
William Nogueira <will.ribeiro@live.com>
2022-04-21 08:51:20 -0300

    criando repositório de especificação

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,22 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log('TODO');
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest) {
+    const specificationAlreadExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadExists) {
+      throw new Error('Specification already exists');
+    }
+
+    this.specificationsRepository.create({ name, description });
   }
 }
 
