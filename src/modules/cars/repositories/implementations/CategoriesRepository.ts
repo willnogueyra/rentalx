200f526784a0cbcbe0995ab6cda684b2f83d0c52
William Nogueira <will.ribeiro@live.com>
2022-04-27 17:11:42 -0300

    separando os repositórios

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,8 +1,8 @@
-import { Category } from '../model/Category';
+import { Category } from '../../model/Category';
 import {
   ICategoriesRepository,
   ICreateCategoryDTO,
-} from './ICategoriesService';
+} from '../ICategoriesService';
 
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
