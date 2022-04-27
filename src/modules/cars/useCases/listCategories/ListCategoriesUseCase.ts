556b37fdd6c1b28c9d0ddd89ca400ee796c4a284
William Nogueira <will.ribeiro@live.com>
2022-04-26 23:47:06 -0300

    refatorando a listagem de categoria

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,14 @@
+import { Category } from '../../model/Category';
+import { ICategoriesRepository } from '../../repositories/ICategoriesService';
+
+class ListCategoriesUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute(): Category[] {
+    const categories = this.categoriesRepository.list();
+
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCase };
