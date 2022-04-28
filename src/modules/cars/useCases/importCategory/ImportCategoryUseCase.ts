a8910b8dba66195614b15b93c77ea5f3098513ed
William Nogueira <will.ribeiro@live.com>
2022-04-27 23:56:01 -0300

    conceito de stream

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,22 @@
+import fs from "fs";
+import { parse as csvParse } from "csv-parse"
+
+/* Estamos recebendo arquivo do Controller, 
+  createReadStream criando em uma stream de leitura passando caminho (file.path), 
+  pipe vai pegar pedaço por pedaço e passar para parseFile para ler linha por linha
+*/
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file)
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile)
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    })
   }
 
 }
